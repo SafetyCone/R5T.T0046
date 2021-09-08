391107e34d21375d6d4363e886e8272f356072d8
David Coats <david@getsafetycone.com>
2021-09-08 16:09:27 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0046/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0046
+{
+    /// <summary>
+    /// Construction project for Author type abstraction and implementation (simple)..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
